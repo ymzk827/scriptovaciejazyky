e350631b20e06658f145ff857cb4aecc46ada8be
ymzk827 <emuzyka827@gmail.com>
2025-04-08 11:17:11 +0200

    pripojenie DB (commented)

--- sablona/otazky.php
+++ sablona/otazky.php
@@ -36,10 +36,16 @@ class QnA {
 
     public function readQnA() {
         try {
+            
+            // vytvorenie aj poslanie prikazu "SELECT" ktory vypiše otazky aj odpovede k nim
             $sql = "SELECT otazka, odpoved FROM otazky";
             $stmt = $this->conn->prepare($sql); 
             $stmt->execute();
+
+            // konvertovanie vystupu prikazu SELECT z tabuľky otazok do array v PHP
             $results = $stmt->fetchAll();
+
+            // vytvorenie prvkov v akordeone aj použivanie udajov z DB
 
             foreach ($results as $row) {
                 echo '<div class="accordion">';
